1e74c4dca636ded37cf9b9b10fa87910337dd55d
ness <ness.frame98@gmail.com>
2024-08-09 07:00:18 +0600

    Changed: The logic of rendering posts and sorted posts

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./styles/App.css"
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -11,12 +12,9 @@ function App() {
         {id: 3, title: 'C#', body: 'about the C#!'},
         {id: 4, title: 'C++', body: 'about the C++!'},
     ]);
-
-//  данная функция передается в PostForm
     function createPost(newPost) {
         setPosts([...posts, newPost])
     }
-
     function removePost(post) {
         setPosts(posts.filter(e => e.id !== post.id))
     }
@@ -24,13 +22,21 @@ function App() {
 
 
     const [selectedSort, setSelectedSort] = useState('')
-
     function sortPosts(sort) {
         setSelectedSort(sort)
-    //  функция сорт мутирует объект поэтому мы создаем копию объекта
-        setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
     }
+    function getSortedPosts() {
+        console.log('func worked')
+        if (selectedSort) {
+            return  [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+        }
+        return posts
+    }
+    const sortedPosts = getSortedPosts()
 
+
+
+    const [searchQuery, setSearchQuery] = useState('')
 
 
     return (
@@ -38,20 +44,24 @@ function App() {
             <PostForm create={createPost} posts={posts} />
 
             <hr style={{margin: '15px 0'}}></hr>
+            <div>
+                <MyInput
+                    placeholder={'search...'}
+                    value={searchQuery}
+                    onChange={event => setSearchQuery(event.target.value)}
+                />
 
+                <MySelect
+                    value={selectedSort}
+                    onChange={sortPosts}
 
-
-            <MySelect
-                value={selectedSort}
-                onChange={sortPosts}
-
-                defaultValue = 'Sort by'
-                options={[
-                    {value: 'title', name: 'title'},
-                    {value: 'body', name: 'description'}
-                ]}
-            />
-
+                    defaultValue = 'Sort by'
+                    options={[
+                        {value: 'title', name: 'title'},
+                        {value: 'body', name: 'description'}
+                    ]}
+                />
+            </div>
 
 
             {
@@ -59,7 +69,7 @@ function App() {
             ?  <h1 style={{textAlign: 'center', padding: '20px 0'}}>
                     no Posts yet
                 </h1>
-            :   <PostList remove={removePost} posts={posts} title='Posts:'/>
+            :   <PostList remove={removePost} posts={sortedPosts} title='Posts:'/>
             }
         </>
     );
